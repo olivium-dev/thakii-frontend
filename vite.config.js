7596d93233936de318d5a5f9add2e96c31f63cbc
Ahmad Younes <163556904+ahmad-youness-2005@users.noreply.github.com>
2026-03-18 15:25:55 +0200

    remove extra changes that don't add value

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // ensure assets load correctly when served from site root (e.g. nginx /var/www/thakii-frontend)
   plugins: [react()],
   server: {
     port: 3000,
@@ -25,7 +24,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
-    target: 'es2022',
   },
   preview: {
     port: 3000,
